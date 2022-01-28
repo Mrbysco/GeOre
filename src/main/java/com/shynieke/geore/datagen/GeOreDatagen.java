66e076dc5bf7d02f64f8d1a540e75288eae539e4
Mrbysco <Mrbysco@users.noreply.github.com>
2022-01-28 19:15:03 +0100

    More tags?

--- src/main/java/com/shynieke/geore/datagen/GeOreDatagen.java
+++ src/main/java/com/shynieke/geore/datagen/GeOreDatagen.java
@@ -502,7 +502,11 @@ public class GeOreDatagen {
 			super(dataGenerator, blockTagsProvider, Reference.MOD_ID, existingFileHelper);
 		}
 
-		public static final Tag.Named<Item> GEORE = forgeTag("geore");
+		public static final Tag.Named<Item> GEORE_CLUSTERS = forgeTag("geore_clusters");
+		public static final Tag.Named<Item> GEORE_SMALL_BUDS = forgeTag("geore_small_buds");
+		public static final Tag.Named<Item> GEORE_MEDIUM_BUDS = forgeTag("geore_medium_buds");
+		public static final Tag.Named<Item> GEORE_LARGE_BUDS = forgeTag("geore_large_buds");
+		public static final Tag.Named<Item> GEORE_SHARDS = forgeTag("geore_shards");
 
 		@Override
 		protected void addTags() {
@@ -532,9 +536,25 @@ public class GeOreDatagen {
 		}
 
 		private void addGeore(GeOreBlockReg blockReg) {
-			Tag.Named<Item> georeTag = forgeTag("geore/" + blockReg.getName());
-			this.tag(GEORE).addTag(georeTag);
-			this.tag(georeTag).add(blockReg.getShard().get());
+			Tag.Named<Item> smallBudsTag = forgeTag("geore_small_bud/" + blockReg.getName());
+			this.tag(GEORE_SMALL_BUDS).addTag(smallBudsTag);
+			this.tag(smallBudsTag).add(blockReg.getCluster().get().asItem());
+
+			Tag.Named<Item> mediumBudsTag = forgeTag("geore_medium/" + blockReg.getName());
+			this.tag(GEORE_MEDIUM_BUDS).addTag(mediumBudsTag);
+			this.tag(mediumBudsTag).add(blockReg.getSmallBud().get().asItem());
+
+			Tag.Named<Item> largeBudsTag = forgeTag("geore_large/" + blockReg.getName());
+			this.tag(GEORE_LARGE_BUDS).addTag(largeBudsTag);
+			this.tag(largeBudsTag).add(blockReg.getCluster().get().asItem());
+
+			Tag.Named<Item> clusterTag = forgeTag("geore_cluster/" + blockReg.getName());
+			this.tag(GEORE_CLUSTERS).addTag(clusterTag);
+			this.tag(clusterTag).add(blockReg.getCluster().get().asItem());
+
+			Tag.Named<Item> shardTag = forgeTag("geore_shard/" + blockReg.getName());
+			this.tag(GEORE_SHARDS).addTag(shardTag);
+			this.tag(shardTag).add(blockReg.getShard().get());
 		}
 
 		private static Tag.Named<Item> forgeTag(String name) {
