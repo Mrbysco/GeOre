d88d0db2434b4e20cf042036fbd048cd963da55e
Mrbysco <Mrbysco@users.noreply.github.com>
2026-02-01 23:02:14 +0100

    Create a budding_geore tag

--- src/main/java/com/shynieke/geore/datagen/server/GeOreBlockTagsProvider.java
+++ src/main/java/com/shynieke/geore/datagen/server/GeOreBlockTagsProvider.java
@@ -6,6 +6,7 @@ import com.shynieke.geore.registry.GeOreRegistry;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.core.HolderLookup.Provider;
 import net.minecraft.data.PackOutput;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.BlockTags;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.level.block.Block;
@@ -21,6 +22,9 @@ public class GeOreBlockTagsProvider extends BlockTagsProvider {
 		super(packOutput, lookupProvider, Reference.MOD_ID, existingFileHelper);
 	}
 
+	private static final TagKey<Block> BUDDING_GEORE = modTag("budding_geore");
+	public static final TagKey<Block> NON_MOVABLE = BlockTags.create(ResourceLocation.fromNamespaceAndPath("create", "non_movable"));
+
 	private static TagKey<Block> modTag(String name) {
 		return BlockTags.create(Reference.modLoc(name));
 	}
@@ -32,8 +36,9 @@ public class GeOreBlockTagsProvider extends BlockTagsProvider {
 			this.addCrystalSounds(reg);
 			this.addGeore(reg);
 			this.tag(Tags.Blocks.GLASS_BLOCKS_TINTED).add(reg.getTintedGlass().get());
-			this.tag(Tags.Blocks.RELOCATION_NOT_SUPPORTED).add(reg.getBudding().get());
 		}
+		this.tag(Tags.Blocks.RELOCATION_NOT_SUPPORTED).addTag(BUDDING_GEORE);
+		this.tag(NON_MOVABLE).addTag(BUDDING_GEORE);
 	}
 
 	private void addMineable(GeOreBlockReg blockReg) {
@@ -53,6 +58,7 @@ public class GeOreBlockTagsProvider extends BlockTagsProvider {
 		this.tag(clustersTag).add(blockReg.getCluster().get());
 		this.tag(Tags.Blocks.CLUSTERS).addTag(clustersTag);
 		this.tag(Tags.Blocks.BUDDING_BLOCKS).add(blockReg.getBudding().get());
+		this.tag(BUDDING_GEORE).add(blockReg.getBudding().get());
 
 		TagKey<Block> blockTag = modTag("storage_blocks/" + "geore_" + blockReg.getName());
 		this.tag(blockTag).add(blockReg.getBlock().get());
